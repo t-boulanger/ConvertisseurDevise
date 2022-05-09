da01bf49620c0f52870ffee7d1c7e186aad34010
Thomas BOULANGER <BOULANGER@bts-sio.local>
2022-05-09 15:53:19 +0200

    dernière version 09/05

--- app/src/main/java/com/example/convertisseurdevise/MainActivity.java
+++ app/src/main/java/com/example/convertisseurdevise/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -17,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
     private Spinner spDevise1;
     private Spinner spDevise2;
     private String ChoixDevise;
+    private EditText etDevise1;
+    private EditText etDevise2;
+    private String TextNombre1;
+    private String Nombre2;
+    private int Nombre1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         btConvertir=(Button) findViewById(R.id.btConvertir);
         spDevise1=(Spinner) findViewById(R.id.spDevise1);
         spDevise2=(Spinner) findViewById(R.id.spDevise2);
+        etDevise1=(EditText) findViewById(R.id.etDevise1);
+        etDevise2=(EditText) findViewById(R.id.etDevise2);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Devise, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -37,6 +45,12 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 ChoixDevise = adapterView.getItemAtPosition(i).toString();
                 Toast.makeText(adapter.getContext(), ChoixDevise, Toast.LENGTH_SHORT).show();
+
+                TextNombre1=etDevise1.getText().toString();
+                Nombre1 = Integer.parseInt(TextNombre1);
+
+                etDevise2.setText(Nombre1);
+
             }
 
             @Override
